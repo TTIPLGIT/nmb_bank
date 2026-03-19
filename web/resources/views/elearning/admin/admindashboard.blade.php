85c56e9f2746c05deef1e9b445443f7ff7ba7750
saranyag8 <saranya.g@talentakeaways.com>
2026-03-19 18:00:29 +0530

    dashboard_changes

--- web/resources/views/elearning/admin/admindashboard.blade.php
+++ web/resources/views/elearning/admin/admindashboard.blade.php
@@ -1584,8 +1584,8 @@
                 </div>
             </div>
             <div class="container-fluid course_and_schedule_container" style="margin-top:6%">
-                <div class="card">
-                    <div class="card-body">
+                <div class="card" style="border-radius:10px !important">
+                    <div class="card-body" style="border-radius:10px !important">
                         <div class="d-flex justify-content-between align-items-center mb-3">
 
                             <div>
